00b8305840e96451579a045800d0b4e961638e7c
Marcos Wesley <marcoswesley314@gmail.com>
2020-01-03 01:28:36 -0300

    Add files via upload

---
+++ precache-manifest.8cd29fa02808e659f5364786c5ec7146.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c15d8852127a5348d6f5ec23de54d188",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ac76c0ebe4039cf74c3",
+    "url": "/static/css/2.559fd73f.chunk.css"
+  },
+  {
+    "revision": "88efc3006fa0230c1d7a",
+    "url": "/static/css/main.fef0082e.chunk.css"
+  },
+  {
+    "revision": "0ac76c0ebe4039cf74c3",
+    "url": "/static/js/2.e802171e.chunk.js"
+  },
+  {
+    "revision": "c7abd4a823ee85afac2f05677368b7de",
+    "url": "/static/js/2.e802171e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "88efc3006fa0230c1d7a",
+    "url": "/static/js/main.e32d7dae.chunk.js"
+  },
+  {
+    "revision": "806bbd1a6d0c4b9cc89f",
+    "url": "/static/js/runtime-main.d0210c43.js"
+  },
+  {
+    "revision": "48429817edd773f23811eb6d6861d1fe",
+    "url": "/static/media/img_background.48429817.jpg"
+  },
+  {
+    "revision": "e0efcf6f506e73f3877291f6a857f9da",
+    "url": "/static/media/perfil2.e0efcf6f.png"
+  },
+  {
+    "revision": "2bf8079302e5b726f129524583274e7b",
+    "url": "/static/media/perfil_arredondado.2bf80793.png"
+  }
+]);
